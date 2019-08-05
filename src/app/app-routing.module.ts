3ce94d700a909d6b0bc4380eb216497ff5ece420
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2019-08-05 18:30:51 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "contato", component: ContatoComponent}
+  {path: "contato", component: ContatoComponent},
+  {path: "form", component: FormComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
