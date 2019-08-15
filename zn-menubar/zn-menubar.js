50d674e2d53f9476d7c976f7b2b62a3f4dee316d
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2019-08-15 11:27:26 -0300

    Add files via upload

--- zn-menubar/zn-menubar.js
+++ zn-menubar/zn-menubar.js
@@ -1,26 +1,21 @@
- $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
+$(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
       });
+});
 
       // Scrolling Effect
 
       $(window).on("scroll", function() {
             if($(window).scrollTop()) {
                   $('nav').addClass('black');
+                  $('.batest').css({"color":"white","transition":"1s"});
+
             }
 
             else {
                   $('nav').removeClass('black');
+                  $('.batest').css({"color":"rgba(0, 88, 219, 0.94)","transition":"1s"});
+
             }
       })
-
-      function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-              x.className += " responsive";
-            } else {
-              x.className = "topnav";
-            }
-          }
